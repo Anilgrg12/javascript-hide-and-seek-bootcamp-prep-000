0b5937b646112446025d606e47581bddc6a44400
amusing-transpiler-2573 <anil_grg@hotmail.com>
2019-10-22 04:42:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,4 +5,9 @@ function nestedTarget(){
   return document.querySelector('#nested .target');
 }
 
-function increaseRankBy(n)
+function increaseRankBy(n){
+  var ranks = document.querySelectorAll('ul.ranked-list li');
+  for (var i = 0; i < ranks.length; i++) {
+    ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n
+  }
+}
